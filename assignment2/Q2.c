b952c2b698bacb87be18ca95733b5b5cf2e0e137
kusnure nikita <nikitakusnurendk01@gmail.com>
2024-03-15 15:10:38 -0400

    Q2.c

---
+++ assignment2/Q2.c
@@ -0,0 +1,21 @@
+//Input a number and display whether number is Even or Odd.
+
+#include<stdio.h>
+
+int main()
+{
+	int num1;
+	printf("Enter the number:");
+	scanf("%d",&num1);
+
+	if(num1%2==0)
+	printf("Number is Even\n");
+
+	else
+	printf("Number is odd\n");
+
+
+
+
+return 0;
+}
