49fb507b139ccede184f7887dd174286fb6db1cf
kusnure nikita <nikitakusnurendk01@gmail.com>
2024-03-18 14:29:15 -0400

    Q13.c

---
+++ assignment3/Q13.c
@@ -0,0 +1,32 @@
+//13. Write a program to display n terms of Fibonacci series
+#include <stdio.h>
+
+int main()
+
+{
+
+    int num, a=-1,b=1,c;
+
+    printf("Enter a number: ");
+
+    scanf("%d",&num);
+
+    printf("Fibonacci series: ");
+
+    for(int i=0;i<num;i++)
+
+    {
+
+        c=a+b;
+
+        printf("%d, ",c);
+
+        a=b;
+
+        b=c;
+
+    }
+
+    return 0;
+
+}
